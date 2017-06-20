7fca8997f2478ce3977117a1ca4748a1ecb344d3
Jing Ma <ma.jing@outlook.com>
2017-06-20 22:57:22 +0200

    Update Test.java with streams, lambdas, filter and map.

--- src/main/java/com/wixsite/jingmacv/Test.java
+++ src/main/java/com/wixsite/jingmacv/Test.java
@@ -1,14 +1,45 @@
 package com.wixsite.jingmacv;
 
-import org.openqa.selenium.By;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Test extends WebScraperCassandra {
 
 	public static void main(String[] args) {
-		init("http://demo.guru99.com/selenium/deprecated.html");
-		driver.switchTo().frame("classFrame");
-		System.out.println(driver.getPageSource());
-        driver.findElement(By.linkText("Deprecated")).click();
-        driver.close();
+		// Uses a stream and filters a list of persons using a lambda.
+		List<Person> list = Arrays.asList(new Person("Jing", 27), new Person("Jing", 30), new Person("Floris", 28));
+		Person person = list.stream().filter(p -> p.name.equals("Jing") || p.age == 30).findAny().orElse(null);
+		System.out.println(person.name + " is " + person.age + " years old.");
+		
+		// Uses a .map() function to get the name of a person.
+		list.stream().map(Person::getAge).collect(Collectors.toList()).forEach(System.out::println);
+	}
+	
+	public static class Person {
+		
+		private String name;
+		private int age;
+		
+		public Person(String name, int age) {
+			this.name = name;
+			this.age = age;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
 	}
 }
